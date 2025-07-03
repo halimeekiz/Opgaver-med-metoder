2f584afe0d1c8c40e2647d5f4bbe8dfe73f9fd71
halimeekiz <halime_sen88@hotmail.com>
2025-07-03 12:09:12 +0200

    Første commit

---
+++ Lommeregner/Projektnavn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lommeregner
+{
+    public class Projektnavn
+    {
+        public double Addition(double a, double b)
+
+        {
+            return a + b;
+        }
+
+
+        public double Subtraktions(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiplikations(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divisions(double a, double b)
+        {
+            if (b > 0 && a > b)
+            {
+                return a / b;
+            }
+        }
+    }
+}
